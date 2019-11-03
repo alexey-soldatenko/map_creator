ec72b60c8853d2c6346f21ff5c6479613bfea334
alexey-soldatenko <alexso1datenko86@gmail.com>
2019-11-03 16:09:03 +0200

    Move part of code to separate methods

--- svg.py
+++ svg.py
@@ -1,13 +1,15 @@
 import math
 import xml.etree.ElementTree as ET
 
-IMPORTANT_TAGS = ["highway", "place", "amenity", "shop", "building", "landuse", "natural", "leisure", "office", "waterway", "power"]
+IMPORTANT_TAGS = ["highway", "place", "amenity", "shop", "building",
+                  "landuse", "natural", "leisure", "office", "waterway", "power"]
 
 class SVGElement:
     def __init__(self, name):
         self.name = name
         self._attributes = dict()
         self.childs = []
+        self.coords = []
 
     def to_str(self):
         attr = ['{}="{}"'.format(k, v) for k, v in self._attributes.items()]
@@ -65,64 +67,88 @@ class SVG:
         return ((coords['minlat'], coords['minlon']),
                 (coords['maxlat'], coords['maxlon']))
 
-    def from_xml(self):
-        self.xml_tree = self.parse_xml()
-        ways = self.xml_tree.findall('way')
-        relations = self.get_relations(self.xml_tree.findall('relation'))
-        nodes = {node.attrib['id']:node for node in self.xml_tree.findall('node')}
-        x, y = self.get_map_coords(self.xml_tree)[0]
+    def create_root(self):
         self.root = SVGElement(name='svg')
         self.root.set_attributes({'class': "svg", 'width': self.width, 'height': self.height})
 
-        text_elements = []
-        for way in ways:
-            childs = way.getchildren()
-            line_coords = []
-            el_classes = set()
-            el = SVGElement(name="polyline")
-            way_name = ""
-            for child in childs:
-                if child.tag == "nd":
-                    node = nodes[child.attrib['ref']]
-                    x1, y1 = (float(node.attrib['lat']),
-                              float(node.attrib['lon']))
-                    line_coords.append(self.coords_to_2d(x, y, x1, y1))
-                elif child.tag == 'tag':
-                    if child.attrib['k'] == "name":
-                        way_name = child.attrib['v']
-                    if child.attrib['k'] in IMPORTANT_TAGS:
-                        el_classes.add(child.attrib['k'])
-                        el_classes.add(child.attrib['v'])
-            for key, ids in relations.items():
-                if way.attrib['id'] in ids:
-                    el_classes.add(key)
-                    break
-            if line_coords:
-                points = " ".join(["{},{}".format(*coords)
-                                   for coords in line_coords])
-                el.set_attributes({'class': " ".join(el_classes), 'points': points, "data-name": way_name})
-                if self.is_visible_element(line_coords):
-                    self.root.add_child(el)
-                    if way_name:
-                        text = SVGElement(name="text")
-                        text.set_attributes({'x': line_coords[0][0], 'y': line_coords[0][1], "class": "text"})
-                        text.add_child(SVGText(way_name))
-                        text_elements.append(text)
-        #for text in text_elements:
-        #    self.root.add_child(text)
+    def get_classes_by_element_id(self, element_id):
+        el_classes = set()
+        for class_names, ids in self.relations.items():
+            if element_id in ids:
+                for class_name in class_names:
+                    el_classes.add(class_name)
+        return el_classes
 
-        #self.root.add_child(SVGMarker(200, 200))
+    def get_common_classes(self, tags):
+        el_classes = set()
+        for tag, value in tags.items():
+            if tag in IMPORTANT_TAGS:
+                el_classes |= set([tag, value])
+        return el_classes
 
+    def get_classes_for_highway(self, tags):
+        el_classes = set()
+        lanes = int(tags.get("lanes", 0))
+        nat_ref = tags.get("nat_ref")
+        if lanes >= 2:
+            lanes_class = "highway_big" if lanes > 2 else "highway_middle"
+            el_classes.add(lanes_class)
+        if nat_ref:
+            el_classes.add("national_way")
+        return el_classes
+
+    def create_way_element(self, way):
+        el = SVGElement(name="polyline")
+        line_coords = []
+        el_classes = set()
+        tags = dict()
+        way_name = tags.get("name", "")
+        for child in way.getchildren():
+            if child.tag == "nd":
+                node = self.nodes[child.attrib['ref']]
+                x1, y1 = (float(node.attrib['lat']),
+                          float(node.attrib['lon']))
+                line_coords.append(self.coords_to_2d(self.x, self.y, x1, y1))
+            elif child.tag == 'tag':
+                tags[child.attrib['k']] = child.attrib['v']
+        el_classes |= self.get_common_classes(tags)
+        el_classes |= self.get_classes_by_element_id(way.attrib['id'])
+        el_classes |= self.get_classes_for_highway(tags)
+        if line_coords:
+            el.coords = line_coords
+            points = " ".join(["{},{}".format(*coords)
+                               for coords in line_coords])
+            el.set_attributes({"class": " ".join(el_classes),
+                               "points": points,
+                               "data-name": way_name,
+                               "id": way.attrib["id"]})
+        return el
+
+    def set_data_from_xml(self):
+        self.xml_tree = self.parse_xml()
+        self.ways = self.xml_tree.findall('way')
+        self.relations = self.get_relations(self.xml_tree.findall('relation'))
+        self.nodes = {node.attrib['id']: node for node in self.xml_tree.findall('node')}
+        self.x, self.y = self.get_map_coords(self.xml_tree)[0] 
+                
+    def from_xml(self):
+        self.create_root()
+        self.set_data_from_xml()
+        for way in self.ways:
+            el = self.create_way_element(way)
+            if self.is_visible_element(el):
+                self.root.add_child(el)
+                
     def get_relations(self, xml_relations):
         result = dict()
         for relation in xml_relations:
             members = set()
-            tag = ""
+            tag = tuple()
             for node in relation.getchildren():
                 if node.tag == "member":
                     members.add(node.attrib["ref"])
                 elif node.tag == 'tag' and node.attrib['k'] in IMPORTANT_TAGS:
-                    tag = node.attrib['k']
+                    tag = tag + (node.attrib['k'], node.attrib['v'])
             if tag:
                 result[tag] = result.get(tag, set()) | members
         return result
@@ -132,13 +158,13 @@ class SVG:
         if self.root:
             return self.root.to_str()
 
-    def is_visible_element(self, points, visible_percent=3):
-        points_num = len(points)
+    def is_visible_element(self, element, visible_percent=3):
+        points_num = len(element.coords)
         perimeter = 0
         for i in range(points_num):
             if i < points_num - 1:
-                x1, y1 = points[i]
-                x2, y2 = points[i+1]
+                x1, y1 = element.coords[i]
+                x2, y2 = element.coords[i+1]
                 perimeter += math.sqrt((x2 - x1)**2 + (y1 - y2)**2)
         percent = (perimeter / self.width) * 100
         if percent > visible_percent:
